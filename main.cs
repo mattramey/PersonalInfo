2572860cfe6ac4cd107c488de2a3570314d26e54
Matt Ramey <110432461+mattramey@users.noreply.github.com>
2024-05-22 00:51:38 +0000

    Seccond Commit

--- main.cs
+++ main.cs
@@ -1,12 +1,10 @@
-using System;// this imports something re \read powerpoint
+using System;// this imports the entire System namespace into the program
 
 
 
-namespace PersonNameSpace
-{// open personNameSpace
+namespace PersonNameSpace {// open personNameSpace
 
-    public class Person
-    {// open class person
+    public class Person {// open class person
 // initialize attributes and create getter and setter methods for proper encapulation
         public string Name { get; set; }
         public DateTime DateOfBirth { get; set; }
@@ -14,23 +12,20 @@ namespace PersonNameSpace
         public string CellPhone { get; set; }
 
 // created a method GetPersonalInfo to access attributes
-        public string GetPersonalInfo()
-        {
+        public string GetPersonalInfo() {
 //$" notates string interpolation 
             return
             $"{Name}\n{DateOfBirth.ToString("M/d/yyyy")}\nwork {WorkPhone}\ncell {CellPhone}"; ;
 
-        }// close GetOutput ()
+        }// close GetPersonalInfo ()
     } // close class person
-      // close personNameSpace
+     
 
-    class PersonalInfo
-    {// open class PersonalInfo
-        public static void Main(string[] args)
-        {
+    class PersonalInfo{// open class PersonalInfo
+        public static void Main(string[] args) {
 
 // declare an array of type person  called people and initialize it to set values.
-            Person[] people = new Person[]{
+            Person[] people = new Person[] {// open array person
       new Person{ // person 1
         Name = "Lucinda Potter",
         DateOfBirth = new DateTime(1992,6,24),
@@ -42,7 +37,7 @@ namespace PersonNameSpace
         DateOfBirth = new DateTime(1978,8,15),
         WorkPhone = "000-000-0101",
         CellPhone = "000-000-0189"
-      },
+      },// close person 2
       new Person {// person 3
         Name = "Chris Johnson",
         DateOfBirth = new DateTime(1987,12,1),
@@ -55,12 +50,11 @@ namespace PersonNameSpace
         DateOfBirth = new DateTime(1949,2,27),
         WorkPhone = "000-444-0101",
         CellPhone = "000-444-0189"
-      }
+      }// close person 4
     };//close array
           
 // create a forEach loop to cycle through each person element in the person[]
-            foreach (var person in people)
-            {// open foreach loop
+            foreach (var person in people){// open foreach loop
               // call the GetOutput() to preint out the information for each person
                 Console.WriteLine(person.GetPersonalInfo());
                 Console.WriteLine();// print a blank line after each itteration of the foreach loop
